0e37349017f713ff04fc36bca0e5123dc8fe4c6f
Trigger App Builder <akhiakmtr12@gmail.com>
2026-09-22 23:55:13 +0000

    fix(compile): add clip + RoundedCornerShape imports

--- app/src/main/java/com/trigger/app/chats/presentation/all_chats/screens/AllChatsScreen.kt
+++ app/src/main/java/com/trigger/app/chats/presentation/all_chats/screens/AllChatsScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Add
 import androidx.compose.material.icons.rounded.Email
@@ -35,6 +36,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
